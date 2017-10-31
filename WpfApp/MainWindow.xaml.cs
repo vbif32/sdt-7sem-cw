3700aec73ebba89d235ac649750025940e82f32b
Михаил Федин <vbif.3618@gmail.com>
2017-10-31 17:50:14 +0300

    form commit

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -11,5 +11,32 @@ namespace WpfApp
         {
             InitializeComponent();
         }
+
+        private void TeacherMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var editTeacherWindow = new EditTeacherWindow();
+            editTeacherWindow.ShowDialog();
+        }
+
+        private void PositionsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var editPostWindow = new EditPostWindow();
+            editPostWindow.ShowDialog();
+        }
+
+        private void ResetDistribution_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetSubjects_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Open101MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
